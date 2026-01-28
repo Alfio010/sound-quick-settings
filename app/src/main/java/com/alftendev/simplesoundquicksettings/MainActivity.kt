553665f237aae7269dae5308a7ae02f4b87356b0
Alfio <fulmine448@gmail.com>
2026-01-28 11:12:36 +0100

    Sound state on app

--- app/src/main/java/com/alftendev/simplesoundquicksettings/MainActivity.kt
+++ app/src/main/java/com/alftendev/simplesoundquicksettings/MainActivity.kt
@@ -1,19 +1,69 @@
 package com.alftendev.simplesoundquicksettings
 
+import android.media.AudioManager
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.alftendev.simplesoundquicksettings.Utils.isDoNotDisturbPermissionGranted
 import com.alftendev.simplesoundquicksettings.Utils.requestDoNotDisturbPermission
+import com.google.android.material.imageview.ShapeableImageView
 
 class MainActivity : AppCompatActivity() {
     private fun setTextView() {
         val textView = findViewById<TextView>(R.id.tvDND)
+        val imageView = findViewById<ShapeableImageView>(R.id.ivSoundMode)
 
         textView.text = if (isDoNotDisturbPermissionGranted(this)) {
             getString(R.string.completed)
         } else {
             getString(R.string.not_completed)
+        }
+
+        val audio = getSystemService(AUDIO_SERVICE) as AudioManager
+
+        imageView.setImageResource(
+            when (audio.ringerMode) {
+                AudioManager.RINGER_MODE_NORMAL -> {
+                    R.drawable.baseline_notifications_active_24
+                }
+
+                AudioManager.RINGER_MODE_VIBRATE -> {
+                    R.drawable.baseline_vibration_24
+                }
+
+                AudioManager.RINGER_MODE_SILENT -> {
+                    R.drawable.baseline_notifications_off_24
+                }
+
+                else -> {
+                    R.drawable.baseline_vibration_24
+                }
+
+            }
+        )
+
+        imageView.setOnClickListener {
+            audio.ringerMode = when (audio.ringerMode) {
+                AudioManager.RINGER_MODE_NORMAL -> {
+                    imageView.setImageResource(R.drawable.baseline_vibration_24)
+                    AudioManager.RINGER_MODE_VIBRATE
+                }
+
+                AudioManager.RINGER_MODE_VIBRATE -> {
+                    imageView.setImageResource(R.drawable.baseline_notifications_off_24)
+                    audio.ringerMode = AudioManager.RINGER_MODE_NORMAL
+                    AudioManager.RINGER_MODE_SILENT
+                }
+
+                AudioManager.RINGER_MODE_SILENT -> {
+                    imageView.setImageResource(R.drawable.baseline_notifications_active_24)
+                    AudioManager.RINGER_MODE_NORMAL
+                }
+
+                else -> {
+                    return@setOnClickListener
+                }
+            }
         }
     }
 
